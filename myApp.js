1b7e00b5b55b24b9e22704ade4446ee4fe01af11
ns200310 <nsenaweera28@gmail.com>
2022-05-14 03:05:23 +0530

    json

--- myApp.js
+++ myApp.js
@@ -5,6 +5,9 @@ app.use('/public',express.static(__dirname+'/public'))
 app.get('/',(req,res) => { 
     res.sendFile(__dirname + "/views/index.html")
  })
+ app.get('/json', (req, res) => {
+   res.json({"message": "Hello json"})
+ })
 
 
 
